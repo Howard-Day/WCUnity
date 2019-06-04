b2f71d13662769a32a4cfeaee740624d367ed9dc
Jan Simon <simonj@hochschule-trier.de>
2019-06-04 18:59:12 +0200

    Added basic CockpitController that supports steering

---
+++ Assets/Scripts/CockpitController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CockpitController : MonoBehaviour
+{
+  public float targetSpeed;
+
+  [SerializeField] float turnRate = 0.5f;
+  [SerializeField] bool invertYAxis = false;
+
+  void Update()
+  {
+    DoSteering();
+    DoThrottle();
+  }
+
+  void DoSteering()
+  {
+    var yaw = ((Input.mousePosition.x / Screen.width) * 2f - 1f) * turnRate;
+    var pitch = ((Input.mousePosition.y / Screen.height) * 2f - 1f) * turnRate;
+
+    transform.localRotation *= Quaternion.AngleAxis(yaw, Vector3.up) * Quaternion.AngleAxis(pitch, invertYAxis ? Vector3.right : Vector3.left);
+  }
+
+  void DoThrottle()
+  {
+    transform.position += transform.forward * targetSpeed * Time.deltaTime; // TODO: this introduces a lot of shakiness
+  }
+}
