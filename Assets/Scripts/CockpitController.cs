5d425a8102b720ec3077c54eb8cedbf5e6708689
Jan Simon <simonj@hochschule-trier.de>
2019-06-05 13:17:01 +0200

    Added improved throttle control to CockpitController, featuring acceleration (+), deceleration (-) and full stop (backspace)

--- Assets/Scripts/CockpitController.cs
+++ Assets/Scripts/CockpitController.cs
@@ -2,10 +2,12 @@
 
 public class CockpitController : MonoBehaviour
 {
-  public float targetSpeed;
+  public float targetSpeed = 15f;
 
-  [SerializeField] float turnRate = 0.5f;
+  [SerializeField] float turnRate = 50f;
   [SerializeField] bool invertYAxis = false;
+  [SerializeField] float speedSelectionSpeed = 10f;
+  [SerializeField] float topSpeed = 20f;
 
   void Update()
   {
@@ -15,14 +17,34 @@ public class CockpitController : MonoBehaviour
 
   void DoSteering()
   {
-    var yaw = ((Input.mousePosition.x / Screen.width) * 2f - 1f) * turnRate;
-    var pitch = ((Input.mousePosition.y / Screen.height) * 2f - 1f) * turnRate;
+    var yaw = ((Input.mousePosition.x / Screen.width) * 2f - 1f) * turnRate * Time.deltaTime;
+    var pitch = ((Input.mousePosition.y / Screen.height) * 2f - 1f) * turnRate * Time.deltaTime;
 
     transform.localRotation *= Quaternion.AngleAxis(yaw, Vector3.up) * Quaternion.AngleAxis(pitch, invertYAxis ? Vector3.right : Vector3.left);
   }
 
   void DoThrottle()
   {
-    transform.position += transform.forward * targetSpeed * Time.deltaTime; // TODO: this introduces a lot of shakiness
+    var fullStop = Input.GetKey(KeyCode.Backspace);
+    var accelerate = Input.GetKey(KeyCode.KeypadPlus);
+    var decelerate = Input.GetKey(KeyCode.KeypadMinus);
+    
+    if (fullStop)
+    {
+      targetSpeed = 0f;
+    }
+    else
+    {
+      if (accelerate && !decelerate)
+      {
+        targetSpeed = Mathf.Min(targetSpeed + speedSelectionSpeed * Time.deltaTime, topSpeed);
+      }
+      else if (decelerate && !accelerate)
+      {
+        targetSpeed = Mathf.Max(targetSpeed - speedSelectionSpeed * Time.deltaTime, 0f);
+      }
+    }
+
+    transform.position += transform.forward * targetSpeed * Time.deltaTime;
   }
 }
