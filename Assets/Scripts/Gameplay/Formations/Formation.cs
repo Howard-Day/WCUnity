39080de202f0807a7364989029cb8e4a238e1262
Kevin Foley <kevinfoley@users.noreply.github.com>
2025-10-29 18:21:29 -0700

    Start work on code for formations; add several formation templates (Finger-Four, Echelon, Diamond, Column)

---
+++ Assets/Scripts/Gameplay/Formations/Formation.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Formation
+{
+    #region FIELDS
+    private TEAM team;
+    private FormationTemplate template;
+    private List<ShipSettings> ships;
+    private int leaderIndex = 0;
+    #endregion
+
+    #region CONSTRUCTORS
+    public Formation(TEAM team, FormationTemplate template)
+    {
+        this.team = team;
+        this.ships = new List<ShipSettings>(template.MaxShips);
+        this.Template = template;
+    }
+
+    public Formation(TEAM team, FormationTemplate template, List<ShipSettings> ships)
+    {
+        this.team = team;
+        this.ships = ships ?? throw new ArgumentNullException(nameof(ships));
+        this.Template = template;
+    }
+    #endregion
+
+    #region PROPERTIES
+    public TEAM Team => team;
+
+    public FormationTemplate Template { 
+        get =>  template; 
+        set
+        {
+            if (value == null) throw new ArgumentException(nameof(value));
+            template = value;
+
+            if (ships.Count > template.MaxShips)
+            {
+                OMEPLogger.Log(this, $"Trimming formation from {ships.Count} to {template.MaxShips}");
+                while (ships.Count > template.MaxShips)
+                {
+                    ships.RemoveAt(ships.Count - 1);
+                }
+            } else
+            {
+                while (ships.Count < template.MaxShips)
+                {
+                    ships.Add(null);
+                }
+            }
+        }
+    }
+
+    public int LeaderIndex
+    {
+        get => leaderIndex;
+        set
+        {
+            if (value < 0 || value >= ships.Count) throw new System.ArgumentOutOfRangeException(nameof(value), $"{value} is out of range 0 - {ships.Count - 1}");
+            leaderIndex = value;
+        }
+    }
+
+    public bool IsFull => !ships.Contains(null);
+    #endregion
+
+    public void SetShipAt(ShipSettings ship, int index)
+    {
+        if (index < 0 || index >= ships.Count) throw new System.ArgumentOutOfRangeException(nameof(index), $"{index} is out of range 0 - {ships.Count - 1}");
+        if (ship != null && ship.Team != team) throw new System.InvalidOperationException($"Cannot add {ship} on team {ship.Team} to formation for team {team}");
+
+        ships[index] = ship;
+
+        if (ship == null && leaderIndex == index)
+        {
+            FindNewLeader();
+        }
+    }
+
+    private void FindNewLeader()
+    {
+        for (int i = 0; i < ships.Count; i++)
+        {
+            if (ships[i] != null)
+            {
+                leaderIndex = i;
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Formations/FormationManager.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationManager : MonoBehaviour
+{
+    private List<Formation> confedFormations = new List<Formation>(10);
+    private List<Formation> kilrathiFormations = new List<Formation>(10);
+    private List<Formation> pirateFormations = new List<Formation>(10);
+    private List<Formation> neutralFormations = new List<Formation>(10);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
